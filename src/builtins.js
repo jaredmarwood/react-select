62bea7e509545ff7c4ffdbcf0e17ec9a1e88cc34
Jared Marwood <14319979+jaredmarwood@users.noreply.github.com>
2019-03-11 07:53:42 +1100

    Update builtins.js

--- src/builtins.js
+++ src/builtins.js
@@ -2,11 +2,11 @@
 
 import type { GroupType, OptionType } from './types';
 
-export const formatGroupLabel = (group: GroupType): string => group.label?group.label:'';
+export const formatGroupLabel = (group: GroupType): string => group?group.label:'';
 
-export const getOptionLabel = (option: OptionType): string => option.label?option.label:'';
+export const getOptionLabel = (option: OptionType): string => option?option.label:'';
 
-export const getOptionValue = (option: OptionType): string => option.value?option.value:'';
+export const getOptionValue = (option: OptionType): string => option?option.value:'';
 
 export const isOptionDisabled = (option: OptionType): boolean =>
   !!option.isDisabled;
